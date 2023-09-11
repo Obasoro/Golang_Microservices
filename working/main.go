320eb847d393aa716a149fd04ab88f004f98a207
Obasoro <obasorokunle@gmail.com>
2023-09-11 13:38:09 +0000

    update main.go

--- working/main.go
+++ working/main.go
@@ -1,15 +1,24 @@
 package main
 
-import "net/http"
+import {
+
+	"net/http"
+	"log"
+	"io/ioutil"
+
+}
 
 func main(){
 
-	http.HandleFunc(*/*, func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc(*/*, func(rw http.ResponseWriter, r *http.Request) {
 		log.Println("Hello World")
+		d, _ := ioutil.ReadAll(r.Body)
 
-	})
+		log.Printf("Data %s\n", d)
 
-	http.HandleFunc(*/*, func(w http.ResponseWriter, r *http.Request) {
+	}
+
+	http.HandleFunc(*/learning, func(w http.ResponseWriter, r *http.Request) {
 		log.Println("Learning Go")
 
 	})
