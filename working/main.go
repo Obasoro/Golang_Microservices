8436c07d059cb081e169bb0b0434ebe9affeb222
Obasoro <obasorokunle@gmail.com>
2023-09-11 13:56:39 +0000

    update

--- working/main.go
+++ working/main.go
@@ -13,8 +13,17 @@ func main(){
 	http.HandleFunc(*/*, func(rw http.ResponseWriter, r *http.Request) {
 		log.Println("Hello World")
 		d, _ := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(rw, "Ooops", http.StatusBadRequest)
+			// rw.WriteHeder(http.StatusBadRequest)
+			// rw.Write([]bytes("Ooops"))
+			return
+		})
 
-		log.Printf("Data %s\n", d)
+		// log.Printf("Data %s\n", d)
+
+
+		fmt.Fprintf("rw, Hello %s", )
 
 	}
 
